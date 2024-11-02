22fa1959aaf1941b62975183594eb37c69d6e7b4
Kaden C <63522451+Kadition@users.noreply.github.com>
2024-11-02 00:12:00 -0500

    snap back functionality

--- Assets/Scripts/Seed.cs
+++ Assets/Scripts/Seed.cs
@@ -6,7 +6,14 @@ public class Seed : MonoBehaviour
 {
     private Vector3 offset;
     private bool isHeld = false;
+    private Vector2 startingPosition;
+    public int seedType;
 
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+    
     // Update is called once per frame
     void Update()
     {
@@ -20,10 +27,13 @@ public class Seed : MonoBehaviour
     {
         offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         isHeld = true;
+        seedType = 1;
     }
 
     private void OnMouseUp()
     {
         isHeld = false;
+        transform.position = startingPosition;
+        seedType = 0;
     }
 }
